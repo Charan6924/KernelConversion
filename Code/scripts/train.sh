9077335ffd52cc9c2d957e3a7d1500247bb45627
Charan6924 <altmrcharbot@gmail.com>
2026-06-12 06:51:34 -0400

    changes

---
+++ Code/scripts/train.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+#SBATCH --job-name=kernel_train
+#SBATCH --account=dlw
+#SBATCH --partition=gpu
+#SBATCH --gres=gpu:1
+#SBATCH --mem=64gb
+#SBATCH --constraint=gpu2h100
+#SBATCH --output=logs/train_%j.out
+#SBATCH --error=logs/train_%j.err
+
+
+# Activate virtual environment if you have one
+# source ~/venv/bin/activate
+
+# Navigate to code directory
+cd /home/cxv166/PhantomTesting/Code
+
+# Print job info
+echo "Job ID: $SLURM_JOB_ID"
+echo "Node: $SLURM_NODELIST"
+echo "GPU: $CUDA_VISIBLE_DEVICES"
+echo "Start time: $(date)"
+
+# Run training
+uv run training/FullTrainLoop.py
+
+echo "End time: $(date)"
---
+++ Code/scripts/train_cycle_gan.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+#SBATCH --job-name=cycle_gan_train
+#SBATCH --partition=cgpudlw
+#SBATCH --nodelist=cgput004
+#SBATCH --gres=gpu:1
+#SBATCH --cpus-per-task=6    
+#SBATCH --mem=120G           
+#SBATCH --time=72:00:00
+#SBATCH --output=/home/cxv166/PhantomTesting/logs/%j_train.out
+#SBATCH --error=/home/cxv166/PhantomTesting/logs/%j_train.err
+
+# Navigate to code directory
+cd /home/cxv166/PhantomTesting/Code
+
+# Print job info
+echo "Job ID: $SLURM_JOB_ID"
+echo "Node: $SLURM_NODELIST"
+echo "GPU: $CUDA_VISIBLE_DEVICES"
+echo "Start time: $(date)"
+
+# Activate venv
+source /home/cxv166/PhantomTesting/.venv/bin/activate
+
+# Run training
+python training/cycle_gan_train_vanilla.py
+echo "End time: $(date)"
